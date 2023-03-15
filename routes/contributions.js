7f8e25c3c3aac3c7c03c7ef2d1b413cd97372fd4
yusufurur <yusufurur@gmail.com>
2023-03-15 13:17:42 -0400

    contributions route

--- routes/contributions.js
+++ routes/contributions.js
@@ -7,4 +7,20 @@ router.get('/:id', (req, res) => {
       res.json({ contributions })
     })
     .catch((err) => console.log("error for getStoryContributions", err));
+});
+
+router.post('/:id/upVotes', (req, res) => {
+  const userId = req.session.userId;
+  addUpVotes(req.params.id, userId)
+    .then(() => {
+      return getUpVotes(req.params.id);
+    })
+    .then(({rows}) => {
+      res.json({...rows[0]})
+    })
+    .catch(err =>
+      console.log("Error with upvoting", err))
+      res.status(500).json({ error: "Failed to add upvote" });
 })
+    module.exports = router;
+
