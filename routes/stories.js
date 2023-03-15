f18188a5e6b16fa7b4551ef46e23fd94e4558f50
yusufurur <yusufurur@gmail.com>
2023-03-15 18:27:36 -0400

    Story Route

--- routes/stories.js
+++ routes/stories.js
@@ -45,11 +45,20 @@ router.get('/:id', (req, res) => {
     .catch((err) => console.log("Error for Story.findById", err));
 });
 
-router.post('/', (req, res) => {
+router.post('/:id', (req, res) => {
   addContribution({ story_id: req.params.id, name_id: req.session.userid, ...req.body })
     .then((response) => {
       res.redirect('/stories/$(req.params.id')
     })
+});
+
+router.post('/', (req, res) => {
+  const { name_id } = req.body;
+  addStory({ ...req.body, name_id: name_id, userId: req.session.userId })
+    .then(() => {
+      res.redirect('/stories/me');
+    })
+    .catch((err) => console.log("Error for addStory", err));
 });
 
 
