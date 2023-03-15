caee00e9ee17e89e5d6bb9575a7cf2df6732e9b7
yusufurur <yusufurur@gmail.com>
2023-03-15 18:17:32 -0400

    Story Route

--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (res, req) => {
+router.get('/', (req, res) => {
   browseSelectStories(req.session.userId)
     .then((stories) => {
       const templateVars = {
@@ -12,3 +12,37 @@ router.get('/', (res, req) => {
     })
     .catch((error) => console.log("Error for BrowseStory", error));
 })
+
+router.get('/me', (req, res) => {
+  getUserStoriesByUserId(req.session.userId)
+  .then((stories) => {
+    const  templateVars = {
+      stories: stories,
+      user: req.session.userId,
+    }
+    res.render('homepage', templateVars)
+  })
+  .catch((err) => console.log("Error for getUserStoriesByUserId", error));
+})
+
+router.get('/:id', (req, res) => {
+  let templateVars = {
+    user: req.session.userId
+  };
+  Story.findById(req.params.id)
+    .then((story) => {
+      templateVars.story = story;
+    })
+    .then(() => {
+      getStoryContributions(req.params.id)
+        .then((contributions) => {
+          templateVars.contributions = contributions;
+          res.render('readstory', templateVars)
+        })
+        .catch((err) => console.log("Error for getStoriesContributions", err));
+    })
+    .catch((err) => console.log("Error for Story.findById", err));
+});
+
+
+
