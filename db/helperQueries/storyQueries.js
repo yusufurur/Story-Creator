85564e31abe0ebac0392a9de143849cf17d8e873
mahadabdi9 <mahadabdi9@gmail.com>
2023-03-16 15:51:38 -0400

    add helper query in storyQueries

--- db/helperQueries/storyQueries.js
+++ db/helperQueries/storyQueries.js
@@ -14,6 +14,15 @@ const browseStory = () => {
     .catch((err) => console.log("Error for browseStory", err));
 }
 
+const browseSelectStories = (id) => {
+  return db.query("SELECT * FROM stories WHERE NOT name_id = $1;", [id])
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((err) => console.log("Error for browseSelectStories", err));
+}
+
 module.exports = {
-  browseStory
+  browseStory,
+  browseSelectStories
 }
